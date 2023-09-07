f5babc1b218281c60bfc0f8e0f7862c187eae4f6
Naveena91 <138840272+Naveena91@users.noreply.github.com>
2023-09-07 03:42:34 +0000

    Submission for Module10 – Naveena Moddu

---
+++ hos10/mern/hos09-client/src/components/login.js
@@ -0,0 +1,13 @@
+//HOS10A login.js
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="https://solid-bassoon-x5w5rqpr6546h6pp9-5050.app.github.dev/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
